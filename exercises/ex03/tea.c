6f045aa41dbe63c4267a52ad49650ea871cbe6b5
hyegibang <hyegi.bang@students.olin.edu>
2019-02-22 14:35:15 -0500

    ex03

--- exercises/ex03/tea.c
+++ exercises/ex03/tea.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc,char *argv[])
-{
+
+// implementation of tee command
+int main(int argc,char *argv[]){
   int i;
   char c;
   int flag;
@@ -17,6 +18,7 @@ int main(int argc,char *argv[])
         exit(1);
     }
 
+// Checking input argument and updating based on the input
     while ((ch = getopt(argc, argv, "ai")) != -1){
   		switch((char)ch){
   		case 'a':
@@ -27,10 +29,7 @@ int main(int argc,char *argv[])
   		}
   	}
 
-int numFiles = argc - options - 1;
-int fileIndex = options+1;
-
-
+//HFC
 argc -= optind;
 argv += optind;
 
@@ -47,13 +46,22 @@ argv += optind;
 			fputs(intputbuffer, out);
 			fclose(out);
 		}
-}
-
-
+  }
 	return 0;
 }
 
 
 /*
 1. What worked, what slowed you down? What would you do differently next time?
+Understanding the overall concpet was rough for me. I had to do some online
+research and ask friends to clarify the understanding. I found the documentations
+linked in the readme file most helpful as it gave some example, which helped me
+visualize the process. For next time, I would like to actually digest nexessary
+fundamental knowledge in HFC before I start.
+
+2.Compare your solution to this professional implementation or this one.
+Comparing my solution with the professional code, I realized that I only
+handel the most minimal errors whereas the professional code contains every
+detailed possible error. For next time, I would also write more detailed
+explanation of cause of the error.
 */
